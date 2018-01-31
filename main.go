21941e1473f05e2e4f782f961af301f441ebc8d5
dignifiedquire <dignifiedquire@gmail.com>
2018-01-31 19:13:17 +0100

    expand and cleanup

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package lock
+package fslock
 
 import (
 	"fmt"
@@ -8,14 +8,10 @@ import (
 	"strings"
 	"syscall"
 
-	"gx/ipfs/QmNiJuT8Ja3hMVpBHXv3Q6dwmperaQ6JjLtpMQgMCD7xvx/go-ipfs-util"
-	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
-	lock "gx/ipfs/QmWi28zbQG6B1xfaaWx5cYoLn3kBFU6pQ6GWQNRV5P6dNe/lock"
+	"github.com/ipfs/go-ipfs-util"
+	logging "github.com/ipfs/go-log"
+	lock "go4.org/lock"
 )
-
-// LockFile is the filename of the repo lock, relative to config dir
-// TODO rename repo lock and hide name
-const LockFile = "repo.lock"
 
 // log is the fsrepo logger
 var log = logging.Logger("lock")
@@ -24,24 +20,32 @@ func errPerm(path string) error {
 	return fmt.Errorf("failed to take lock at %s: permission denied", path)
 }
 
-func Lock(confdir string) (io.Closer, error) {
-	return lock.Lock(path.Join(confdir, LockFile))
+// Lock creates the lock.
+func Lock(confdir, lockFile string) (io.Closer, error) {
+	return lock.Lock(path.Join(confdir, lockFile))
 }
 
-func Locked(confdir string) (bool, error) {
+// Locked checks if there is a lock already set.
+func Locked(confdir, lockFile string) (bool, error) {
 	log.Debugf("Checking lock")
-	if !util.FileExists(path.Join(confdir, LockFile)) {
-		log.Debugf("File doesn't exist: %s", path.Join(confdir, LockFile))
+	if !util.FileExists(path.Join(confdir, lockFile)) {
+		log.Debugf("File doesn't exist: %s", path.Join(confdir, lockFile))
 		return false, nil
 	}
-	if lk, err := Lock(confdir); err != nil {
+	if lk, err := Lock(confdir, lockFile); err != nil {
 		// EAGAIN == someone else has the lock
 		if err == syscall.EAGAIN {
-			log.Debugf("Someone else has the lock: %s", path.Join(confdir, LockFile))
+			log.Debugf("Someone else has the lock: %s", path.Join(confdir, lockFile))
 			return true, nil
 		}
 		if strings.Contains(err.Error(), "resource temporarily unavailable") {
-			log.Debugf("Can't lock file: %s.\n reason: %s", path.Join(confdir, LockFile), err.Error())
+			log.Debugf("Can't lock file: %s.\n reason: %s", path.Join(confdir, lockFile), err.Error())
+			return true, nil
+		}
+
+		// we hold the lock ourselves
+		if strings.Contains(err.Error(), "already locked") {
+			log.Debugf("Lock is already held by us: %s", path.Join(confdir, lockFile))
 			return true, nil
 		}
 
